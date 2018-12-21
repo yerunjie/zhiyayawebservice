92a5be986cee9ae8c70e39dcd24904df5f509edf
yerunjie <1102266271@qq.com>
2018-12-21 13:17:52 +0800

    fix

--- src/main/java/com/zhiyaya/zhiyayaweb/configuration/WebMvcConfig.java
+++ src/main/java/com/zhiyaya/zhiyayaweb/configuration/WebMvcConfig.java
@@ -19,6 +19,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authorizationInterceptor);
-        registry.addInterceptor(dbTimesInterceptor);
+        //registry.addInterceptor(dbTimesInterceptor);
     }
 }
